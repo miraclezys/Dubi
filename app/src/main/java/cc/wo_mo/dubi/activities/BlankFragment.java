6f4e8b5ff76e334129313b1b65b1e418ba8f09e9
womo <1060022311@qq.com>
2016-12-11 22:53:59 +0800

    update

---
+++ app/src/main/java/cc/wo_mo/dubi/activities/BlankFragment.java
@@ -0,0 +1,102 @@
+package cc.wo_mo.dubi.activities;
+
+import android.os.Bundle;
+import android.provider.Telephony;
+import android.support.annotation.Nullable;
+import android.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+import cc.wo_mo.dubi.R;
+import cc.wo_mo.dubi.TweetAdapter;
+import cc.wo_mo.dubi.data.ApiClient;
+import cc.wo_mo.dubi.data.DubiService;
+import cc.wo_mo.dubi.data.Model.Tweet;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class BlankFragment extends Fragment {
+    SwipeRefreshLayout mSwipeRefreshLayout;
+    RecyclerView mRecyclerView;
+    LinearLayoutManager layoutManager;
+    int lastVisiblePosition;
+    TweetAdapter mAdapter;
+    List<Tweet> data;
+    DubiService client = ApiClient.getClient();
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        Toolbar toolbar = (Toolbar) getActivity().findViewById(R.id.toolbar);
+        toolbar.setTitle("Fuck1");
+        mSwipeRefreshLayout = (SwipeRefreshLayout)view.findViewById(R.id.swipe_refresh_layout);
+        mSwipeRefreshLayout.setProgressViewOffset(false, 0,
+                (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 24,
+                        getResources().getDisplayMetrics()));
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                getData();
+            }
+        });
+        initRecyclerView(view);
+        getData();
+        return view;
+    }
+
+    void getData() {
+        mSwipeRefreshLayout.setRefreshing(true);
+        client.listAllTweets(-1, 100).enqueue(new Callback<List<Tweet>>() {
+            @Override
+            public void onResponse(Call<List<Tweet>> call, Response<List<Tweet>> response) {
+                if (response.code() == 200) {
+                    mAdapter.mData = response.body();
+                    mAdapter.notifyDataSetChanged();
+                    mSwipeRefreshLayout.setRefreshing(false);
+                    Log.d("tweet", ApiClient.gson.toJson(data));
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Tweet>> call, Throwable t) {
+
+            }
+        });
+    }
+
+    void initRecyclerView(View view) {
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
+        layoutManager = new LinearLayoutManager(view.getContext());
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        mRecyclerView.setLayoutManager(layoutManager);
+        mAdapter = new TweetAdapter(view.getContext(), data);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                if (newState == RecyclerView.SCROLL_STATE_IDLE &&
+                        lastVisiblePosition+1 == mAdapter.getItemCount()) {
+
+                }
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                lastVisiblePosition = layoutManager.findLastVisibleItemPosition();
+            }
+        });
+    }
+
+}
