7aec14d4b13ef89526ff334905e0fecba6148db7
SHIN <fuyixin6666@outlook.com>
2016-12-12 22:23:44 +0800

    #v1.1 实现了notify（消息提醒）的相关界面基础显示

---
+++ app/src/main/java/cc/wo_mo/dubi/activities/MYNotifyActivity.java
@@ -0,0 +1,44 @@
+package cc.wo_mo.dubi.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import cc.wo_mo.dubi.R;
+/**
+ * Created by SHIN on 2016/12/12.
+ */
+public class MYNotifyActivity extends AppCompatActivity {
+    RecyclerView rv1;
+    List<Map<String,Object>> notifyData;
+    ListView lv1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mynotify);
+
+        rv1 = (RecyclerView)findViewById(R.id.Notiftlist);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        rv1.setLayoutManager(layoutManager);
+
+        notifyData = new ArrayList<>();
+        for(int i = 1;i<=10;i++){
+            Map<String,Object>temp = new LinkedHashMap<>();
+            temp.put("user_name","user_name"+i);
+            for(int j = 1;j<=5;j++) {
+                temp.put("text", "This is the text " + i);
+            }
+            notifyData.add(temp);
+        }
+        MyNotifyAdapter notifyAdapter = new MyNotifyAdapter(MYNotifyActivity.this,notifyData);
+        rv1.setAdapter(notifyAdapter);
+    }
+}
