9e211a41947bf8cf8ae4e817cd58ad43c2239c79
shushu <907896638@qq.com>
2016-12-15 02:12:57 +0800

    个人信息编辑界面使用了对话框显示

--- app/src/main/java/cc/wo_mo/dubi/activities/MyInfoActivity.java
+++ app/src/main/java/cc/wo_mo/dubi/activities/MyInfoActivity.java
@@ -1,10 +1,14 @@
 package cc.wo_mo.dubi.activities;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 import cc.wo_mo.dubi.R;
@@ -22,8 +26,24 @@ public class MyInfoActivity extends AppCompatActivity {
         mEditButton.setOnClickListener(new View.OnClickListener(){
             @Override
              public void onClick(View view){
-                Intent intent = new Intent(MyInfoActivity.this,MyInfoEditActivity.class);
-                startActivity(intent);
+                LayoutInflater factory = LayoutInflater.from(MyInfoActivity.this);
+                final View view1 = factory.inflate(R.layout.i_information_edit, null);
+                AlertDialog.Builder builder1 = new AlertDialog.Builder(MyInfoActivity.this);
+                builder1.setView(view1);
+                builder1.setPositiveButton("保存修改", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                })
+                        .setNegativeButton("取消修改", new DialogInterface.OnClickListener(){
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                            }
+                        })
+                        .create();
+                builder1.show();
             }
         });
     }
