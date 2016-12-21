24e281d8ad3fd9fce3ce52ea839ae7e2f59051ab
shushu <907896638@qq.com>
2016-12-21 13:12:08 +0800

    修改了登录界面

--- app/src/main/java/cc/wo_mo/dubi/activities/LoginActivity.java
+++ app/src/main/java/cc/wo_mo/dubi/activities/LoginActivity.java
@@ -3,8 +3,11 @@ package cc.wo_mo.dubi.activities;
 import android.content.Intent;
 import android.app.ActionBar;
 import android.graphics.Color;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -30,8 +33,8 @@ public class LoginActivity extends AppCompatActivity {
     Button mSignUpButton;
     EditText mUsername;
     EditText mPassword;
-    View mSignInView;
-    View mSignUpView;
+    TextInputLayout mNameInput;
+    TextInputLayout mPasswordInput;
     DubiService client;
     int state = SIGN_IN;
     @Override
@@ -44,48 +47,115 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void init() {
-        mSubmitButton = (Button) findViewById(R.id.submit_button);
+       // mSubmitButton = (Button) findViewById(R.id.submit_button);
         mSignInButton = (Button) findViewById(R.id.sign_in_button);
         mSignUpButton = (Button) findViewById(R.id.sign_up_button);
         mUsername = (EditText) findViewById(R.id.name_edit);
         mPassword = (EditText) findViewById(R.id.password_one_edit);
-        mSignInView = (View) findViewById(R.id.in_view);
-        mSignUpView = (View) findViewById(R.id.up_view);
-        mSignInView.setBackgroundColor(getResources().getColor(R.color.colorAccent));
-        mSignUpView.setVisibility(View.INVISIBLE);
-        mSignInButton.setTextColor(getResources().getColor(R.color.colorAccent));
+       // mSignInButton.setTextColor(getResources().getColor(R.color.colorAccent));
+        mNameInput = (TextInputLayout)findViewById(R.id.name_text_input);
+        mPasswordInput = (TextInputLayout)findViewById(R.id.password_text_input);
     }
 
     private void setButtonListener() {
         mSignInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mSignInButton.setTextColor(getResources().getColor(R.color.colorAccent));
-                mSignUpButton.setTextColor(getResources().getColor(R.color.colorGray));
-                mSignInView.setVisibility(View.VISIBLE);
-                mSignUpView.setVisibility(View.INVISIBLE);
-                mSignInView.setBackgroundColor(getResources().getColor(R.color.colorAccent));
-                state = SIGN_IN;
+                String username = mUsername.getEditableText().toString();
+                String password = mPassword.getEditableText().toString();
+                if(TextUtils.isEmpty(username)){
+                    mNameInput.setErrorEnabled(true);
+                    mNameInput.setError("用户名不能为空");
+                }
+                else if(TextUtils.isEmpty(password)){
+                    mPasswordInput.setErrorEnabled(true);
+                    mPasswordInput.setError("密码不能为空");
+                }
+                else{
+                    mNameInput.setErrorEnabled(false);
+                    mPasswordInput.setErrorEnabled(false);
+                    client.login(username, password).enqueue(new Callback<LoginResponse>() {
+                        @Override
+                        public void onResponse(Call<LoginResponse> call,
+                                               Response<LoginResponse> response) {
+                            LoginResponse res = response.body();
+                            if (response.code() == 200) {
+                              //  showToast("Sign in success");
+                                Snackbar.make(mSignInButton,"Sign in success",Snackbar.LENGTH_SHORT)
+                                        .setDuration(1000)
+                                        .show();
+                                ApiClient.token = res.token;
+                                ApiClient.user_id = res.user_id;
+                                Intent intent = new Intent();
+                                intent.setClass(LoginActivity.this, MainActivity.class);
+                                startActivity(intent);
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Call<LoginResponse> call, Throwable t) {
+                            t.printStackTrace();
+                        }
+                    });
+                }
             }
         });
 
         mSignUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mSignInButton.setTextColor(getResources().getColor(R.color.colorGray));
-                mSignUpButton.setTextColor(getResources().getColor(R.color.colorAccent));
-                mSignInView.setVisibility(View.INVISIBLE);
-                mSignUpView.setVisibility(View.VISIBLE);
-                mSignUpView.setBackgroundColor(getResources().getColor(R.color.colorAccent));
-                state = SIGN_UP;
+                String username = mUsername.getEditableText().toString();
+                String password = mPassword.getEditableText().toString();
+                if(TextUtils.isEmpty(username)){
+                    mNameInput.setErrorEnabled(true);
+                    mNameInput.setError("用户名不能为空");
+                }
+                else if(TextUtils.isEmpty(password)){
+                    mPasswordInput.setErrorEnabled(true);
+                    mPasswordInput.setError("密码不能为空");
+                }
+                else{
+                    mNameInput.setErrorEnabled(false);
+                    mPasswordInput.setErrorEnabled(false);
+                    client.registration(username, password).enqueue(new Callback<BaseResponse>() {
+                        @Override
+                        public void onResponse(Call<BaseResponse> call,
+                                               Response<BaseResponse> response) {
+                            BaseResponse res = response.body();
+                            if (res == null)
+                                try {
+                                    res = ApiClient.gson.fromJson(response.errorBody().string(),
+                                            BaseResponse.class);
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            Snackbar.make(mSignInButton,res.message,Snackbar.LENGTH_SHORT)
+                                    .setDuration(1000)
+                                    .show();
+                        }
+
+                        @Override
+                        public void onFailure(Call<BaseResponse> call, Throwable t) {
+                            t.printStackTrace();
+                        }
+                    });
+                }
             }
         });
 
-        mSubmitButton.setOnClickListener(new View.OnClickListener() {
+    /*    mSubmitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String username = mUsername.getEditableText().toString();
                 String password = mPassword.getEditableText().toString();
+                if(TextUtils.isEmpty(username)){
+                    mNameInput.setErrorEnabled(true);
+                    mNameInput.setError("用户名不能为空");
+                }
+                else if(TextUtils.isEmpty(password)){
+                    mPasswordInput.setErrorEnabled(true);
+                    mPasswordInput.setError("密码不能为空");
+                }
                 if (state == SIGN_IN) {
                     client.login(username, password).enqueue(new Callback<LoginResponse>() {
                         @Override
@@ -130,7 +200,7 @@ public class LoginActivity extends AppCompatActivity {
                     });
                 }
             }
-        });
+        });*/
     }
 
     void showToast(String msg) {
